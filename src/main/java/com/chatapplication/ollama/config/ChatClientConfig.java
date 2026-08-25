3548035b55b1bbac31d7c687a6d09cb6c96f3580
shubhi <shubhiengineer20@gmail.com>
2026-08-25 22:26:08 +0530

    Rest APIs for chat and email using Prompt Template

---
+++ src/main/java/com/chatapplication/ollama/config/ChatClientConfig.java
@@ -0,0 +1,24 @@
+package com.chatapplication.ollama.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
+        return  chatClientBuilder
+                .defaultSystem("""
+                        You are an internal HR assistant. Your role is to help\s
+                        employees with questions related to HR policies such as\s
+                        leave policies, working hours, benefits and code of conduct.
+                        If a user asks for help with anything outside this,\s
+                        kindly inform that you can assist with queries related to\s
+                        HR policies.
+                        """)
+                .defaultUser("How can you help me ?")
+                .build();
+    }
+}
